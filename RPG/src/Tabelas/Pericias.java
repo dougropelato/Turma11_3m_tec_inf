6880d306a59c266bea1e29b77765e5c19a751f63
Aloucurae <alexj.desantanna@gmail.com>
2015-05-06 00:08:38 -0300

    adicionado a classe pericias

---
+++ RPG/src/Tabelas/Pericias.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tabelas;
+
+/**
+ *
+ * @author Alexjonas
+ */
+public class Pericias {
+    
+    int codigo_pericia;
+    String nome_pericia;
+
+    public int getCodigo_pericia() {
+        return codigo_pericia;
+    }
+
+    public void setCodigo_pericia(int codigo_pericia) {
+        this.codigo_pericia = codigo_pericia;
+    }
+
+    public String getNome_pericia() {
+        return nome_pericia;
+    }
+
+    public void setNome_pericia(String nome_pericia) {
+        this.nome_pericia = nome_pericia;
+    }
+    
+}
