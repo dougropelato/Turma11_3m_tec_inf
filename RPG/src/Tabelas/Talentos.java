aed539bd0ea2ec4b3a44ae3788a3bf65e32d7d81
Aloucurae <alexj.desantanna@gmail.com>
2015-05-06 00:11:00 -0300

    adicionado classe talentos

---
+++ RPG/src/Tabelas/Talentos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tabelas;
+
+/**
+ *
+ * @author Alexjonas
+ */
+public class Talentos {
+
+    public int getCodigo_talento() {
+        return codigo_talento;
+    }
+
+    public void setCodigo_talento(int codigo_talento) {
+        this.codigo_talento = codigo_talento;
+    }
+
+    public String getNome_codigo() {
+        return nome_codigo;
+    }
+
+    public void setNome_codigo(String nome_codigo) {
+        this.nome_codigo = nome_codigo;
+    }
+    
+    int codigo_talento;
+    String nome_codigo;
+    
+}
