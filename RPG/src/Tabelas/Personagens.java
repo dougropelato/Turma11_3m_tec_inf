7cab72f59f7c301ad0c629ee5184b8df8ace67e1
FelipeTrakiinaS <felipealexandre.ittner@gmail.com>
2015-05-12 21:49:01 -0700

    Adicionado classe Personagens

---
+++ RPG/src/Tabelas/Personagens.java
@@ -0,0 +1,195 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tabelas;
+
+/**
+ *
+ * @author Felipe e Adriel
+ */
+public class Personagens {
+
+    public int getCodigo_personagem() {
+        return codigo_personagem;
+    }
+
+    public void setCodigo_personagem(int codigo_personagem) {
+        this.codigo_personagem = codigo_personagem;
+    }
+
+    public int getCodigo_classe() {
+        return codigo_classe;
+    }
+
+    public void setCodigo_classe(int codigo_classe) {
+        this.codigo_classe = codigo_classe;
+    }
+
+    public String getNome_personagem() {
+        return nome_personagem;
+    }
+
+    public void setNome_personagem(String nome_personagem) {
+        this.nome_personagem = nome_personagem;
+    }
+
+    public int getIdade_personagem() {
+        return idade_personagem;
+    }
+
+    public void setIdade_personagem(int idade_personagem) {
+        this.idade_personagem = idade_personagem;
+    }
+
+    public int getPeso_personagem() {
+        return peso_personagem;
+    }
+
+    public void setPeso_personagem(int peso_personagem) {
+        this.peso_personagem = peso_personagem;
+    }
+
+    public Double getAltura_personagem() {
+        return altura_personagem;
+    }
+
+    public void setAltura_personagem(Double altura_personagem) {
+        this.altura_personagem = altura_personagem;
+    }
+
+    public int getForca_personagem() {
+        return forca_personagem;
+    }
+
+    public void setForca_personagem(int forca_personagem) {
+        this.forca_personagem = forca_personagem;
+    }
+
+    public int getDestreza_personagem() {
+        return destreza_personagem;
+    }
+
+    public void setDestreza_personagem(int destreza_personagem) {
+        this.destreza_personagem = destreza_personagem;
+    }
+
+    public int getContituicao_personagem() {
+        return contituicao_personagem;
+    }
+
+    public void setContituicao_personagem(int contituicao_personagem) {
+        this.contituicao_personagem = contituicao_personagem;
+    }
+
+    public int getInteligencia_personagem() {
+        return inteligencia_personagem;
+    }
+
+    public void setInteligencia_personagem(int inteligencia_personagem) {
+        this.inteligencia_personagem = inteligencia_personagem;
+    }
+
+    public int getSabedoria_personagem() {
+        return sabedoria_personagem;
+    }
+
+    public void setSabedoria_personagem(int sabedoria_personagem) {
+        this.sabedoria_personagem = sabedoria_personagem;
+    }
+
+    public int getCarisma_personagem() {
+        return carisma_personagem;
+    }
+
+    public void setCarisma_personagem(int carisma_personagem) {
+        this.carisma_personagem = carisma_personagem;
+    }
+
+    public int getFortitude_personagem() {
+        return fortitude_personagem;
+    }
+
+    public void setFortitude_personagem(int fortitude_personagem) {
+        this.fortitude_personagem = fortitude_personagem;
+    }
+
+    public int getReflexos_personagem() {
+        return reflexos_personagem;
+    }
+
+    public void setReflexos_personagem(int reflexos_personagem) {
+        this.reflexos_personagem = reflexos_personagem;
+    }
+
+    public int getVontade_personagem() {
+        return vontade_personagem;
+    }
+
+    public void setVontade_personagem(int vontade_personagem) {
+        this.vontade_personagem = vontade_personagem;
+    }
+
+    public int getClasse_armadura_personagem() {
+        return classe_armadura_personagem;
+    }
+
+    public void setClasse_armadura_personagem(int classe_armadura_personagem) {
+        this.classe_armadura_personagem = classe_armadura_personagem;
+    }
+
+    public int getPontos_vida_personagem() {
+        return pontos_vida_personagem;
+    }
+
+    public void setPontos_vida_personagem(int pontos_vida_personagem) {
+        this.pontos_vida_personagem = pontos_vida_personagem;
+    }
+
+    public int getIniciativa_personagem() {
+        return iniciativa_personagem;
+    }
+
+    public void setIniciativa_personagem(int iniciativa_personagem) {
+        this.iniciativa_personagem = iniciativa_personagem;
+    }
+
+    public int getBase_ataque_personagem() {
+        return base_ataque_personagem;
+    }
+
+    public void setBase_ataque_personagem(int base_ataque_personagem) {
+        this.base_ataque_personagem = base_ataque_personagem;
+    }
+
+    public int getCodigo_raca() {
+        return codigo_raca;
+    }
+
+    public void setCodigo_raca(int codigo_raca) {
+        this.codigo_raca = codigo_raca;
+    }
+    
+    int codigo_personagem;
+    int codigo_classe;
+    String nome_personagem;
+    int idade_personagem;
+    int peso_personagem;
+    Double altura_personagem;
+    int forca_personagem;
+    int destreza_personagem;
+    int contituicao_personagem;
+    int inteligencia_personagem;
+    int sabedoria_personagem;
+    int carisma_personagem;
+    int fortitude_personagem;
+    int reflexos_personagem;
+    int vontade_personagem;
+    int classe_armadura_personagem;
+    int pontos_vida_personagem;
+    int iniciativa_personagem;
+    int base_ataque_personagem;
+    int codigo_raca;
+    
+}
