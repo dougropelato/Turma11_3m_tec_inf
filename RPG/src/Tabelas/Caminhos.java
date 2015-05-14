c4f47191361bac46fa224b3358fa7a0764958f42
Kaayo <caio_figurski@hotmail.com>
2015-05-13 23:38:29 -0300

    Criada a Classes Caminhos e Posições... (CAIO)
    Serviço Feito!! É Nóis...

---
+++ RPG/src/Tabelas/Caminhos.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tabelas;
+
+/**
+ *
+ * @author Caio
+ */
+public class Caminhos {
+    int codigo_camino;
+    String nome_caminho;
+    int codigo_missao;
+    int codigo_mapa;
+
+    public int getCodigo_camino() {
+        return codigo_camino;
+    }
+
+    public void setCodigo_camino(int codigo_camino) {
+        this.codigo_camino = codigo_camino;
+    }
+
+    public String getNome_caminho() {
+        return nome_caminho;
+    }
+
+    public void setNome_caminho(String nome_caminho) {
+        this.nome_caminho = nome_caminho;
+    }
+
+    public int getCodigo_missao() {
+        return codigo_missao;
+    }
+
+    public void setCodigo_missao(int codigo_missao) {
+        this.codigo_missao = codigo_missao;
+    }
+
+    public int getCodigo_mapa() {
+        return codigo_mapa;
+    }
+
+    public void setCodigo_mapa(int codigo_mapa) {
+        this.codigo_mapa = codigo_mapa;
+    }
+    
+}
---
+++ RPG/src/Tabelas/Posicoes.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tabelas;
+
+/**
+ *
+ * @author Caio
+ */
+public class Posicoes {
+    int codigo_posicao;
+    String descricao_posicao;
+    int coordenadaX_posicao;
+    int coordenadaY_posicao;
+    int codigo_caminho;
+    int codigo_npc;
+
+    public int getCodigo_posicao() {
+        return codigo_posicao;
+    }
+
+    public void setCodigo_posicao(int codigo_posicao) {
+        this.codigo_posicao = codigo_posicao;
+    }
+
+    public String getDescricao_posicao() {
+        return descricao_posicao;
+    }
+
+    public void setDescricao_posicao(String descricao_posicao) {
+        this.descricao_posicao = descricao_posicao;
+    }
+
+    public int getCoordenadaX_posicao() {
+        return coordenadaX_posicao;
+    }
+
+    public void setCoordenadaX_posicao(int coordenadaX_posicao) {
+        this.coordenadaX_posicao = coordenadaX_posicao;
+    }
+
+    public int getCoordenadaY_posicao() {
+        return coordenadaY_posicao;
+    }
+
+    public void setCoordenadaY_posicao(int coordenadaY_posicao) {
+        this.coordenadaY_posicao = coordenadaY_posicao;
+    }
+
+    public int getCodigo_caminho() {
+        return codigo_caminho;
+    }
+
+    public void setCodigo_caminho(int codigo_caminho) {
+        this.codigo_caminho = codigo_caminho;
+    }
+
+    public int getCodigo_npc() {
+        return codigo_npc;
+    }
+
+    public void setCodigo_npc(int codigo_npc) {
+        this.codigo_npc = codigo_npc;
+    }
+}
