1665ec4cdd2d59dfda22aaad4520045a6e2ac9ef
FelipeTrakiinaS <felipealexandre.ittner@gmail.com>
2015-05-13 21:07:46 -0700

    Adicionado a classe N/N JogadoresPersonagens

--- RPG/src/Tabelas/JogadoresPersonagens.java
+++ RPG/src/Tabelas/JogadoresPersonagens.java
@@ -10,5 +10,23 @@ package Tabelas;
  * @author Felipe e Adriel
  */
 public class JogadoresPersonagens {
+
+    public int getCodigo_jogador() {
+        return codigo_jogador;
+    }
+
+    public void setCodigo_jogador(int codigo_jogador) {
+        this.codigo_jogador = codigo_jogador;
+    }
+
+    public int getCodigo_personagem() {
+        return codigo_personagem;
+    }
+
+    public void setCodigo_personagem(int codigo_personagem) {
+        this.codigo_personagem = codigo_personagem;
+    }
+    int codigo_jogador;
+    int codigo_personagem;
     
 }
