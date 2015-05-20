09599d98d25b4a9c2587ee99c7b1a2c8987d6978
Jefferson luiz da Silva <jeff.hiei@gmail.com>
2015-05-20 20:32:57 -0300

    Criado classeClasse, seus atributos e getters e setters

---
+++ RPG/src/Tabelas/ProgressaoTalento.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tabelas;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class ProgressaoTalento {
+    int codigo_progressao;
+    int codigo_talento;
+}
---
+++ RPG/src/Tabelas/Progressoes.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tabelas;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class Progressoes {
+    int codigo_progressao;
+    int nivel_progressao;
+    int bonus_base_progressao;
+    int fortitude_progressao;
+    int reflexos_progressao;
+    int vontade_progressao;
+    int pericia_progressao;
+    int quantidade_dado_progressao;
+    int tipo_dado_progressao;
+    int bonus_ca_progressao;
+    int classes_codigo_classe;
+
+    public int getCodigo_progressao() {
+        return codigo_progressao;
+    }
+
+    public void setCodigo_progressao(int codigo_progressao) {
+        this.codigo_progressao = codigo_progressao;
+    }
+
+    public int getNivel_progressao() {
+        return nivel_progressao;
+    }
+
+    public void setNivel_progressao(int nivel_progressao) {
+        this.nivel_progressao = nivel_progressao;
+    }
+
+    public int getBonus_base_progressao() {
+        return bonus_base_progressao;
+    }
+
+    public void setBonus_base_progressao(int bonus_base_progressao) {
+        this.bonus_base_progressao = bonus_base_progressao;
+    }
+
+    public int getFortitude_progressao() {
+        return fortitude_progressao;
+    }
+
+    public void setFortitude_progressao(int fortitude_progressao) {
+        this.fortitude_progressao = fortitude_progressao;
+    }
+
+    public int getReflexos_progressao() {
+        return reflexos_progressao;
+    }
+
+    public void setReflexos_progressao(int reflexos_progressao) {
+        this.reflexos_progressao = reflexos_progressao;
+    }
+
+    public int getVontade_progressao() {
+        return vontade_progressao;
+    }
+
+    public void setVontade_progressao(int vontade_progressao) {
+        this.vontade_progressao = vontade_progressao;
+    }
+
+    public int getPericia_progressao() {
+        return pericia_progressao;
+    }
+
+    public void setPericia_progressao(int pericia_progressao) {
+        this.pericia_progressao = pericia_progressao;
+    }
+
+    public int getQuantidade_dado_progressao() {
+        return quantidade_dado_progressao;
+    }
+
+    public void setQuantidade_dado_progressao(int quantidade_dado_progressao) {
+        this.quantidade_dado_progressao = quantidade_dado_progressao;
+    }
+
+    public int getTipo_dado_progressao() {
+        return tipo_dado_progressao;
+    }
+
+    public void setTipo_dado_progressao(int tipo_dado_progressao) {
+        this.tipo_dado_progressao = tipo_dado_progressao;
+    }
+
+    public int getBonus_ca_progressao() {
+        return bonus_ca_progressao;
+    }
+
+    public void setBonus_ca_progressao(int bonus_ca_progressao) {
+        this.bonus_ca_progressao = bonus_ca_progressao;
+    }
+
+    public int getClasses_codigo_classe() {
+        return classes_codigo_classe;
+    }
+
+    public void setClasses_codigo_classe(int classes_codigo_classe) {
+        this.classes_codigo_classe = classes_codigo_classe;
+    }
+}
