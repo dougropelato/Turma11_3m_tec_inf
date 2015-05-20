f0fe6be5c028c2f7791a5f366140719cd7e02239
Jefferson luiz da Silva <jeff.hiei@gmail.com>
2015-05-20 20:05:59 -0300

    Criado classeClasse, seus atributos e getters e setters

---
+++ RPG/src/Tabelas/Classes.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tabelas;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class Classes {
+    int codigo_classe;
+    String descricao_classe;
+    String  nome_classe;
+    int dado_vida_classe;
+    int usar_armaduras_classe;
+    int usar_armas_classe;
+    int quantidade_pericias_classe;
+    int codigo_armadura;
+    int dinheiro_classe;
+    int codigo_arma;
+    int codigo_escudo;
+
+    public int getCodigo_classe() {
+        return codigo_classe;
+    }
+
+    public void setCodigo_classe(int codigo_classe) {
+        this.codigo_classe = codigo_classe;
+    }
+
+    public String getDescricao_classe() {
+        return descricao_classe;
+    }
+
+    public void setDescricao_classe(String descricao_classe) {
+        this.descricao_classe = descricao_classe;
+    }
+
+    public String getNome_classe() {
+        return nome_classe;
+    }
+
+    public void setNome_classe(String nome_classe) {
+        this.nome_classe = nome_classe;
+    }
+
+    public int getDado_vida_classe() {
+        return dado_vida_classe;
+    }
+
+    public void setDado_vida_classe(int dado_vida_classe) {
+        this.dado_vida_classe = dado_vida_classe;
+    }
+
+    public int getUsar_armaduras_classe() {
+        return usar_armaduras_classe;
+    }
+
+    public void setUsar_armaduras_classe(int usar_armaduras_classe) {
+        this.usar_armaduras_classe = usar_armaduras_classe;
+    }
+
+    public int getUsar_armas_classe() {
+        return usar_armas_classe;
+    }
+
+    public void setUsar_armas_classe(int usar_armas_classe) {
+        this.usar_armas_classe = usar_armas_classe;
+    }
+
+    public int getQuantidade_pericias_classe() {
+        return quantidade_pericias_classe;
+    }
+
+    public void setQuantidade_pericias_classe(int quantidade_pericias_classe) {
+        this.quantidade_pericias_classe = quantidade_pericias_classe;
+    }
+
+    public int getCodigo_armadura() {
+        return codigo_armadura;
+    }
+
+    public void setCodigo_armadura(int codigo_armadura) {
+        this.codigo_armadura = codigo_armadura;
+    }
+
+    public int getDinheiro_classe() {
+        return dinheiro_classe;
+    }
+
+    public void setDinheiro_classe(int dinheiro_classe) {
+        this.dinheiro_classe = dinheiro_classe;
+    }
+
+    public int getCodigo_arma() {
+        return codigo_arma;
+    }
+
+    public void setCodigo_arma(int codigo_arma) {
+        this.codigo_arma = codigo_arma;
+    }
+
+    public int getCodigo_escudo() {
+        return codigo_escudo;
+    }
+
+    public void setCodigo_escudo(int codigo_escudo) {
+        this.codigo_escudo = codigo_escudo;
+    }
+}
