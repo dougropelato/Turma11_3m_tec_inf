735eda1af868f33a37de43c753bba2e53e01e3b3
OrlandoBitencourt <orlando.cechlar@gmail.com>
2015-05-27 18:57:53 -0300

    "salvar" dos forms caminhos e posições/posicoesnpcs
    além disso criei os getters e setter da tabeça posicoesNpcs

--- RPG/src/Tabelas/PosicoesNpcs.java
+++ RPG/src/Tabelas/PosicoesNpcs.java
@@ -10,5 +10,22 @@ package tabelas;
  * @author Usuario5
  */
 public class PosicoesNpcs {
-    
+    int codigo_posicoes;
+    int codigo_npc;
+
+    public int getCodigo_posicoes() {
+        return codigo_posicoes;
+    }
+
+    public void setCodigo_posicoes(int codigo_posicoes) {
+        this.codigo_posicoes = codigo_posicoes;
+    }
+
+    public int getCodigo_npc() {
+        return codigo_npc;
+    }
+
+    public void setCodigo_npc(int codigo_npc) {
+        this.codigo_npc = codigo_npc;
+    }
 }
