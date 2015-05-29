ed45e9beafee31859ac4f2c9bdc005ae38a29a3b
GuilhermeVicenti <guilherme.svicenti@hotmail.com>
2015-05-29 09:40:28 -0300

    alteração tabela raca.
    tabela raca.

---
+++ RPG/src/Tabelas/Raca.java
@@ -0,0 +1,126 @@
+
+package Tabelas;
+
+
+public class Raca {
+    
+    int codigo_raca;
+    String descricao_raca;
+    int forca_raca;
+    int destreza_raca;
+    int constituicao_raca;
+    int inteligencia_raca;
+    int sabedoria_raca;
+    int carisma_raca;
+     int qtd_talen_raca_adc;
+     int uso_arma_raca;
+     Boolean uso_escudo_raca;
+     int  armadura_raca_;
+     String nome_raca;
+
+    public int getCodigo_raca() {
+        return codigo_raca;
+    }
+
+    public void setCodigo_raca(int codigo_raca) {
+        this.codigo_raca = codigo_raca;
+    }
+
+    public String getDescricao_raca() {
+        return descricao_raca;
+    }
+
+    public void setDescricao_raca(String descricao_raca) {
+        this.descricao_raca = descricao_raca;
+    }
+
+    public int getForca_raca() {
+        return forca_raca;
+    }
+
+    public void setForca_raca(int forca_raca) {
+        this.forca_raca = forca_raca;
+    }
+
+    public int getDestreza_raca() {
+        return destreza_raca;
+    }
+
+    public void setDestreza_raca(int destreza_raca) {
+        this.destreza_raca = destreza_raca;
+    }
+
+    public int getConstituicao_raca() {
+        return constituicao_raca;
+    }
+
+    public void setConstituicao_raca(int constituicao_raca) {
+        this.constituicao_raca = constituicao_raca;
+    }
+
+    public int getInteligencia_raca() {
+        return inteligencia_raca;
+    }
+
+    public void setInteligencia_raca(int inteligencia_raca) {
+        this.inteligencia_raca = inteligencia_raca;
+    }
+
+    public int getSabedoria_raca() {
+        return sabedoria_raca;
+    }
+
+    public void setSabedoria_raca(int sabedoria_raca) {
+        this.sabedoria_raca = sabedoria_raca;
+    }
+
+    public int getCarisma_raca() {
+        return carisma_raca;
+    }
+
+    public void setCarisma_raca(int carisma_raca) {
+        this.carisma_raca = carisma_raca;
+    }
+
+    public int getQtd_talen_raca_adc() {
+        return qtd_talen_raca_adc;
+    }
+
+    public void setQtd_talen_raca_adc(int qtd_talen_raca_adc) {
+        this.qtd_talen_raca_adc = qtd_talen_raca_adc;
+    }
+
+    public int getUso_arma_raca() {
+        return uso_arma_raca;
+    }
+
+    public void setUso_arma_raca(int uso_arma_raca) {
+        this.uso_arma_raca = uso_arma_raca;
+    }
+
+    public Boolean getUso_escudo_raca() {
+        return uso_escudo_raca;
+    }
+
+    public void setUso_escudo_raca(Boolean uso_escudo_raca) {
+        this.uso_escudo_raca = uso_escudo_raca;
+    }
+
+    public int getArmadura_raca_() {
+        return armadura_raca_;
+    }
+
+    public void setArmadura_raca_(int armadura_raca_) {
+        this.armadura_raca_ = armadura_raca_;
+    }
+
+    public String getNome_raca() {
+        return nome_raca;
+    }
+
+    public void setNome_raca(String nome_raca) {
+        this.nome_raca = nome_raca;
+    }
+     
+    
+}
