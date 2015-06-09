78f202cf1ae5fa2af8e0e12cb0743aafa6f4d5dd
Eduardo Dalpiaz <eduardo.mdalpiaz@gmail.com>
2015-06-09 19:26:59 -0300

    descrição classe dados

--- RPG/src/Utilitários/Dados.java
+++ RPG/src/Utilitários/Dados.java
@@ -13,11 +13,33 @@ import java.util.Random;
  */
 public class Dados {
 
-    public int getDado(int ladosDado) {//ladoDados é  igual a quantidades de lados do dado que vc quer 
+   //Os Dados do Jogo são: //
+   //          d4          //
+   //          d6          //
+   //          d8          //
+   //          d10         //
+   //          d12         //
+   //          d20         //
+     
+   //O Código funciona assim, sempre que precisar utilizar um dado será 
+   //necessário chamá-lo da seguinte maneira na classe:
+        
+   //Dados dAD = new Dados();          //chamar a classe Dados
+    
+   //System.out.println(dAD.getDado(20)); //Dentro do getDado() será necessário
+                                          //informar o valor correspondente ao 
+                                          //dado que voce precisa, por exemplo,
+                                          //um d20 (dAD.getDado(20));
+       
+    
+    public int getDado(int ladosDado) {
+              //ladoDados é  igual a quantidades de lados do dado que vc precisa 
+        
         Random Dado = new Random();
         int numeroAleatorio = 0;
         for (int i = 0; i < 1; i++) {//mostra sequencia de um numero aleatorio
-            numeroAleatorio = Dado.nextInt(ladosDado) + 1;  //numero entre 1 e a quantidade que vc informou
+            numeroAleatorio = Dado.nextInt(ladosDado) + 1;  
+                            //numero entre 1 e a quantidade que vc informou
            
         }
          return numeroAleatorio; // retorna o valor que voce tirou no dado 
