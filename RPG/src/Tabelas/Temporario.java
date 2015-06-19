4fe05ffa4a27bed696d9908de24d69523e7895bc
Aloucurae <alexj.desantanna@gmail.com>
2015-06-18 22:07:37 -0300

    temporarop e batalaha

--- RPG/src/Tabelas/Temporario.java
+++ RPG/src/Tabelas/Temporario.java
@@ -10,5 +10,95 @@ package Tabelas;
  * @author mamut
  */
 public class Temporario {
+    int codigo_temporario;
+    int pontos_vida_temporario;
+    int dinheiro_classe;
+    int codigo_classe;
+    int codigo_personagem;
+    int ca_personagem;
+    int dano_personagem;
+    int codigo_escudo;
+    int codigo_arma;
+    int codigo_consumivel;
+
+    public int getCodigo_escudo() {
+        return codigo_escudo;
+    }
+
+    public void setCodigo_escudo(int codigo_escudo) {
+        this.codigo_escudo = codigo_escudo;
+    }
+
+    public int getCodigo_arma() {
+        return codigo_arma;
+    }
+
+    public void setCodigo_arma(int codigo_arma) {
+        this.codigo_arma = codigo_arma;
+    }
+
+    public int getCodigo_consumivel() {
+        return codigo_consumivel;
+    }
+
+    public void setCodigo_consumivel(int codigo_consumivel) {
+        this.codigo_consumivel = codigo_consumivel;
+    }
     
+
+    public int getCodigo_temporario() {
+        return codigo_temporario;
+    }
+
+    public void setCodigo_temporario(int codigo_temporario) {
+        this.codigo_temporario = codigo_temporario;
+    }
+
+    public int getPontos_vida_temporario() {
+        return pontos_vida_temporario;
+    }
+
+    public void setPontos_vida_temporario(int pontos_vida_temporario) {
+        this.pontos_vida_temporario = pontos_vida_temporario;
+    }
+
+    public int getDinheiro_classe() {
+        return dinheiro_classe;
+    }
+
+    public void setDinheiro_classe(int dinheiro_classe) {
+        this.dinheiro_classe = dinheiro_classe;
+    }
+
+    public int getCodigo_classe() {
+        return codigo_classe;
+    }
+
+    public void setCodigo_classe(int codigo_classe) {
+        this.codigo_classe = codigo_classe;
+    }
+
+    public int getCodigo_personagem() {
+        return codigo_personagem;
+    }
+
+    public void setCodigo_personagem(int codigo_personagem) {
+        this.codigo_personagem = codigo_personagem;
+    }
+
+    public int getCa_personagem() {
+        return ca_personagem;
+    }
+
+    public void setCa_personagem(int ca_personagem) {
+        this.ca_personagem = ca_personagem;
+    }
+
+    public int getDano_personagem() {
+        return dano_personagem;
+    }
+
+    public void setDano_personagem(int dano_personagem) {
+        this.dano_personagem = dano_personagem;
+    }
 }
--- RPG/src/Utilitários/Batalhas.java
+++ RPG/src/Utilitários/Batalhas.java
@@ -6,11 +6,13 @@
 package Utilitários;
 
 import Tabelas.NpcsCombatentes;
+import Tabelas.Temporario;
 import dao.GenericDAO;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import tabelas.Escudos;
 import tabelas.Personagens;
 
 /**
@@ -18,48 +20,114 @@ import tabelas.Personagens;
  * @author mamut
  */
 public class Batalhas {
-    
-    public String ParametroBatalho(Personagens per, NpcsCombatentes npc) throws SQLException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException, InstantiationException, ClassNotFoundException {
-    String ini = "";
 
-    utilitários.Dados dad = new utilitários.Dados();
-    GenericDAO gda = new GenericDAO();
-    Personagens psg = new Personagens();
+    public String ParametroBatalho(Personagens per, NpcsCombatentes npc, Temporario tem) throws SQLException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException, InstantiationException, ClassNotFoundException {
+        String ini = "";
 
-    int vlriniciativaper;
-    int vlriniciativanpc = 0;
+        utilitários.Dados dad = new utilitários.Dados();
+        GenericDAO gda = new GenericDAO();
+        Personagens psg = new Personagens();
+        Temporario tempnpc = new Temporario();
 
-    vlriniciativaper = dad.getDado(20) + per.getDestreza_personagem();
+        int vlriniciativaper;
+        int vlriniciativanpc = 0;
 
-    List<Object> lis = new ArrayList();
+        vlriniciativaper = dad.getDado(20) + per.getDestreza_personagem();
 
-    psg.setCodigo_personagem(npc.getCodigo_personagem());
+        List<Object> lis = new ArrayList();
 
-    lis = gda.listar2(Personagens.class, psg);
+        psg.setCodigo_personagem(npc.getCodigo_personagem());
 
-    for (Object li : lis) {
-        Personagens pso = (Personagens) li;
-        vlriniciativanpc = dad.getDado(20) + pso.getDestreza_personagem();
-    }
+        lis = gda.listar2(Personagens.class, psg);
 
-    if (vlriniciativaper > vlriniciativanpc) {
+        for (Object li : lis) {
+            Personagens pso = (Personagens) li;
+            vlriniciativanpc = dad.getDado(20) + pso.getDestreza_personagem();
 
-    } else {
+            psg.setAltura_personagem(pso.getAltura_personagem());
+            psg.setBase_ataque_personagem(pso.getBase_ataque_personagem());
+            psg.setCarisma_personagem(pso.getCarisma_personagem());
+            psg.setClasse_armadura_personagem(pso.getClasse_armadura_personagem());
+            psg.setContituicao_personagem(pso.getContituicao_personagem());
+            psg.setDestreza_personagem(pso.getDestreza_personagem());
+            psg.setForca_personagem(pso.getForca_personagem());
+            psg.setFortitude_personagem(pso.getFortitude_personagem());
+            psg.setIdade_personagem(pso.getIdade_personagem());
+            psg.setIniciativa_personagem(pso.getIniciativa_personagem());
+            psg.setInteligencia_personagem(pso.getInteligencia_personagem());
+            psg.setNome_personagem(pso.getNome_personagem());
+            psg.setPeso_personagem(pso.getPeso_personagem());
+            psg.setPontos_vida_personagem(pso.getPontos_vida_personagem());
+            psg.setReflexos_personagem(pso.getReflexos_personagem());
+            psg.setSabedoria_personagem(pso.getSabedoria_personagem());
+            psg.setVontade_personagem(pso.getVontade_personagem());
+        }
 
-    }
-       
-                
-                
-                
-                
-                
-                
-                
+        tempnpc.setCodigo_personagem(npc.getCodigo_personagem());
+        lis = gda.listar2(Temporario.class, tempnpc);
+
+        for (Object li : lis) {
+            Temporario tp = (Temporario) li;
+            tempnpc.setCa_personagem(tp.getCa_personagem());
+            tempnpc.setCodigo_arma(tp.getCodigo_arma());
+            tempnpc.setPontos_vida_temporario(tp.getPontos_vida_temporario());
+            tempnpc.setCodigo_escudo(tp.getCodigo_escudo());
+        }
+
+        if (vlriniciativaper > vlriniciativanpc) {
+            if (verAcerto(psg, tempnpc) > verDefesa(per, tem)) {
+
+            } else {
+                ini = "Você Errou.";
                 return ini;
+            }
+
+        } else {
+            if (verAcerto(per, tem) > verDefesa(psg, tempnpc)) {
+
+            } else {
+                ini = "NPC errou.";
+                return ini;
+            }
+        }
+        return ini;
+    }
+
+    public int verAcerto(Personagens pp, Temporario tp) {
+        int res = 0;
+
+        utilitários.Dados dad = new utilitários.Dados();
+
+        if (pp.getModForca() > pp.getModDestreza()) {
+            res = dad.getDado(20) + pp.getModForca() + pp.getBase_ataque_personagem();
+
+        } else {
+            res = dad.getDado(20) + pp.getModDestreza() + pp.getBase_ataque_personagem();
+        }
+
+        return res;
 
     }
 
-    
-    
-    
+    public int verDefesa(Personagens pp, Temporario tp) throws SQLException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException, InstantiationException {
+        int res = 0;
+
+        utilitários.Dados dad = new utilitários.Dados();
+        GenericDAO gda = new GenericDAO();
+        Escudos es = new Escudos();
+
+        es.setCodigo_escudo(tp.getCodigo_arma());
+
+        List<Object> escudoss = gda.listar2(Escudos.class, es);
+
+        for (Object ess : escudoss) {
+            Escudos es1 = (Escudos) ess;
+            es.setBonus_maximo_escudo(es1.getBonus_maximo_escudo());
+        }
+
+        res = (int) (10 + pp.getModDestreza() + es.getBonus_maximo_escudo());
+
+        return res;
+    }
+
 }
