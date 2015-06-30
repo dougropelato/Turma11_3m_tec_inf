0b8ec2350571d759e9929026a2aaa707db4adf22
Aloucurae <alexj.desantanna@gmail.com>
2015-06-30 20:36:40 -0300

    combates autentica

--- RPG/src/Utilitários/Batalhas.java
+++ RPG/src/Utilitários/Batalhas.java
@@ -24,96 +24,59 @@ import tabelas.Personagens;
 public class Batalhas {
 
     public String iniciaBatalha(Personagens per, NpcsCombatentes npc, Temporario tem) throws SQLException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException, InstantiationException, ClassNotFoundException {
-       
+
         Autenticacao auth = Autenticacao.getInstance();
-        
+
         String ini = "";
         utilitários.Dados dad = new utilitários.Dados();
         GenericDAO gda = new GenericDAO();
         Personagens psg = new Personagens();
         Temporario tempnpc = new Temporario();
         int dano = 0;
+        
+        auth.setIniciativa_npc(dano);
+        tem.setIniciativa_personagem(dad.getDado(20) + per.getDestreza_personagem());
 
-        if (tem.getIniciativa_personagem() == 0) {
+        List<Object> lis = new ArrayList();
 
-            tem.setIniciativa_personagem(dad.getDado(20) + per.getDestreza_personagem());
+        psg.setCodigo_personagem(npc.getCodigo_personagem());
 
-            List<Object> lis = new ArrayList();
+        lis = gda.listar2(Personagens.class, psg);
 
-            psg.setCodigo_personagem(npc.getCodigo_personagem());
+        for (Object li : lis) {
+            Personagens pso = (Personagens) li;
 
-            lis = gda.listar2(Personagens.class, psg);
-
-            for (Object li : lis) {
-                Personagens pso = (Personagens) li;
-
-                psg.setAltura_personagem(pso.getAltura_personagem());
-                psg.setBase_ataque_personagem(pso.getBase_ataque_personagem());
-                psg.setCarisma_personagem(pso.getCarisma_personagem());
-                psg.setClasse_armadura_personagem(pso.getClasse_armadura_personagem());
-                psg.setContituicao_personagem(pso.getContituicao_personagem());
-                psg.setDestreza_personagem(pso.getDestreza_personagem());
-                psg.setForca_personagem(pso.getForca_personagem());
-                psg.setFortitude_personagem(pso.getFortitude_personagem());
-                psg.setIdade_personagem(pso.getIdade_personagem());
-                psg.setIniciativa_personagem(pso.getIniciativa_personagem());
-                psg.setInteligencia_personagem(pso.getInteligencia_personagem());
-                psg.setNome_personagem(pso.getNome_personagem());
-                psg.setPeso_personagem(pso.getPeso_personagem());
-                psg.setPontos_vida_personagem(pso.getPontos_vida_personagem());
-                psg.setReflexos_personagem(pso.getReflexos_personagem());
-                psg.setSabedoria_personagem(pso.getSabedoria_personagem());
-                psg.setVontade_personagem(pso.getVontade_personagem());
-            }
-
-            tempnpc.setCodigo_personagem(npc.getCodigo_personagem());
-            lis = gda.listar2(Temporario.class, tempnpc);
-
-            for (Object li : lis) {
-                Temporario tp = (Temporario) li;
-                tempnpc.setCa_personagem(tp.getCa_personagem());
-                tempnpc.setCodigo_arma(tp.getCodigo_arma());
-                tempnpc.setPontos_vida_temporario(tp.getPontos_vida_temporario());
-                tempnpc.setCodigo_escudo(tp.getCodigo_escudo());
-                tempnpc.setIniciativa_personagem(dad.getDado(20) + psg.getDestreza_personagem());
-            }
+            psg.setAltura_personagem(pso.getAltura_personagem());
+            psg.setBase_ataque_personagem(pso.getBase_ataque_personagem());
+            psg.setCarisma_personagem(pso.getCarisma_personagem());
+            psg.setClasse_armadura_personagem(pso.getClasse_armadura_personagem());
+            psg.setContituicao_personagem(pso.getContituicao_personagem());
+            psg.setDestreza_personagem(pso.getDestreza_personagem());
+            psg.setForca_personagem(pso.getForca_personagem());
+            psg.setFortitude_personagem(pso.getFortitude_personagem());
+            psg.setIdade_personagem(pso.getIdade_personagem());
+            psg.setIniciativa_personagem(pso.getIniciativa_personagem());
+            psg.setInteligencia_personagem(pso.getInteligencia_personagem());
+            psg.setNome_personagem(pso.getNome_personagem());
+            psg.setPeso_personagem(pso.getPeso_personagem());
+            psg.setPontos_vida_personagem(pso.getPontos_vida_personagem());
+            psg.setReflexos_personagem(pso.getReflexos_personagem());
+            psg.setSabedoria_personagem(pso.getSabedoria_personagem());
+            psg.setVontade_personagem(pso.getVontade_personagem());
         }
 
-        if (tem.getIniciativa_personagem() > tempnpc.getIniciativa_personagem()) {
-            if (verAcerto(psg, tempnpc) > verDefesa(per, tem)) {
-                dano = dano(per, tem);
-                psg.setPontos_vida_personagem(psg.getPontos_vida_personagem() - dano);
+        tempnpc.setCodigo_personagem(npc.getCodigo_personagem());
+        lis = gda.listar2(Temporario.class, tempnpc);
 
-                if (psg.getPontos_vida_personagem() > 0) {
-                    ini = psg.getNome_personagem() + " recebeu um ataque violento \n";
-                    ini += dano + " pontos de dano ";
-                } else {
-                    ini = psg.getNome_personagem() + " recebeu um ataque violento \n";
-                    ini += dano + " pontos de dano e morreu";
-                }
-
-            } else {
-                ini = "Você Errou.";
-                return ini;
-            }
-
-        } else {
-            if (verAcerto(per, tem) > verDefesa(psg, tempnpc)) {
-
-                dano = dano(psg, tempnpc);
-                per.setPontos_vida_personagem(per.getPontos_vida_personagem() - dano);
-
-                if (per.getPontos_vida_personagem() > 0) {
-                    ini = "voce recebeu um ataque violento";
-                } else {
-
-                }
-
-            } else {
-                ini = "NPC errou.";
-                return ini;
-            }
+        for (Object li : lis) {
+            Temporario tp = (Temporario) li;
+            tempnpc.setCa_personagem(tp.getCa_personagem());
+            tempnpc.setCodigo_arma(tp.getCodigo_arma());
+            tempnpc.setPontos_vida_temporario(tp.getPontos_vida_temporario());
+            tempnpc.setCodigo_escudo(tp.getCodigo_escudo());
+            tempnpc.setIniciativa_personagem(dad.getDado(20) + psg.getDestreza_personagem());
         }
+
         return ini;
     }
 
@@ -128,6 +91,48 @@ public class Batalhas {
         } else {
             res = dad.getDado(20) + pp.getModDestreza() + pp.getBase_ataque_personagem();
         }
+        return res;
+    }
+
+    public String combate() {
+        String res = "";
+
+        if (tem.getIniciativa_personagem() > tempnpc.getIniciativa_personagem()) {
+            if (verAcerto(psg, tempnpc) > verDefesa(per, tem)) {
+                dano = dano(per, tem);
+                psg.setPontos_vida_personagem(psg.getPontos_vida_personagem() - dano);
+
+                if (psg.getPontos_vida_personagem() > 0) {
+                    res = psg.getNome_personagem() + " recebeu um ataque violento \n";
+                    res += dano + " pontos de dano ";
+                } else {
+                    res = psg.getNome_personagem() + " recebeu um ataque violento \n";
+                    res += dano + " pontos de dano e morreu";
+                }
+
+            } else {
+                res = "Você Errou.";
+                return res;
+            }
+
+        } else {
+            if (verAcerto(per, tem) > verDefesa(psg, tempnpc)) {
+
+                dano = dano(psg, tempnpc);
+                per.setPontos_vida_personagem(per.getPontos_vida_personagem() - dano);
+
+                if (per.getPontos_vida_personagem() > 0) {
+                    res = "voce recebeu um ataque violento";
+                } else {
+
+                }
+
+            } else {
+                res = "NPC errou.";
+                return res;
+            }
+        }
+
         return res;
     }
 
